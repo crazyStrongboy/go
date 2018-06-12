e91f6bfbbaa6fe260fd1bfb4369f369bb07a8932
crazyStrongboy <13237124836@163.com>
2018-06-12 10:28:29 +0800

    second commit

---
+++ src/deleteFile/db/conn.go
@@ -0,0 +1,75 @@
+// Copyright 2016 The StudyGolang Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+// http://studygolang.com
+// Author: polaris	polaris@studygolang.com
+
+package db
+
+import (
+	"errors"
+	"fmt"
+	."deleteFile/conf"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/go-xorm/core"
+	"github.com/go-xorm/xorm"
+)
+
+var MasterDB *xorm.Engine
+
+var dns string
+
+func InitMysql() {
+	mysqlConfig, err := ConfigFile.GetSection("mysql")
+	if err != nil {
+		fmt.Println("get mysql config error:", err)
+		return
+	}
+
+	fillDns(mysqlConfig)
+
+	// 启动时就打开数据库连接
+	if err = initEngine(); err != nil {
+		panic(err)
+	}
+}
+
+var (
+	ConnectDBErr = errors.New("connect db error")
+	UseDBErr     = errors.New("use db error")
+)
+
+
+func fillDns(mysqlConfig map[string]string) {
+	dns = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=%s&parseTime=True&loc=Local",
+		mysqlConfig["user"],
+		mysqlConfig["password"],
+		mysqlConfig["host"],
+		mysqlConfig["port"],
+		mysqlConfig["dbname"],
+		mysqlConfig["charset"])
+}
+
+func initEngine() error {
+	var err error
+
+	MasterDB, err = xorm.NewEngine("mysql", dns)
+	if err != nil {
+		return err
+	}
+
+	maxIdle := ConfigFile.MustInt("mysql", "max_idle", 2)
+	maxConn := ConfigFile.MustInt("mysql", "max_conn", 10)
+
+	MasterDB.SetMaxIdleConns(maxIdle)
+	MasterDB.SetMaxOpenConns(maxConn)
+
+	showSQL := ConfigFile.MustBool("xorm", "show_sql", false)
+	logLevel := ConfigFile.MustInt("xorm", "log_level", 1)
+
+	MasterDB.ShowSQL(showSQL)
+	MasterDB.Logger().SetLevel(core.LogLevel(logLevel))
+
+	return nil
+}
+
